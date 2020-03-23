b078c6a36c22242e584d4e03b46356b7322867d3
unknown <1994976877@qq.com>
2020-03-23 23:56:09 +0800

    first react

---
+++ src-ht/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {subRoutes} from './router'
+import {Route,Redirect,Switch} from 'react-router-dom'
+import Admin from './components/admin'
+import MyRoute from './components/myroute'
+function App() {
+  return (
+    <div className="App">
+           {sessionStorage.getItem("token")?
+          <Admin>
+              <Switch>
+                {
+                    subRoutes.map((item)=>{
+                      return <MyRoute key={item.path} path={item.path}
+                       component={item.component} roles={item.roles} />
+                    })
+                }
+                {/* 设置默认进入的页面 */}
+                <Redirect from="/home" to="/home/list" exact />
+             </Switch>
+          </Admin>:<Redirect to="/login" />}
+    
+    </div>
+   
+  );
+}
+
+export default App;
